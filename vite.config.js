b62ce02444d5734c1580080061e55696bfd68957
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-08 22:36:34 +0530

    Feature:Github

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/faisal-portfolio',
+  base: '/',
 
   server: {
     // Proxy API calls to the backend Express server during dev
